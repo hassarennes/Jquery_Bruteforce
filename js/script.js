aeda50ebe4722281167a8aa2e6003c5edaeb0ba1
hassarennes <hassanrennes@gmail.com>
2016-08-02 07:33:31 +0200

    code incomplet

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 var dico=[];
 var results={};
 
@@ -9,10 +10,21 @@ $.ajax({
 });
 
 $(document).ajaxComplete(function(){
-	/*
+	
 	for (var i = 0; i < 1; i++) {
 		testPassword(dico[i]);
 	}
-	*/
+	
 	testPassword(dico[0]);
 });
+
+function testPassword(passwd){
+	$.ajax({
+		url: "http://docusland.fr/code-academie/js_bruteforce/index.php",
+		type: "POST",
+		password: passwd,
+		complete: function(result){
+			results[passwd] = result;
+		}
+	});
+}
