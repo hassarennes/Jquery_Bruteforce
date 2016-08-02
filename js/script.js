916692a7fe3257bd82f490ff89c05c28329540d8
hassarennes <hassanrennes@gmail.com>
2016-08-02 07:30:28 +0200

    essaie de la boucle

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
+var dico=[];
+var results={};
+
 $.ajax({
 	url: "http://docusland.fr/code-academie/js_bruteforce/data/dico.txt",
 	complete: function(result){
 		dico = result.responseText.split("\n");
 	}
+});
+
+$(document).ajaxComplete(function(){
+	/*
+	for (var i = 0; i < 1; i++) {
+		testPassword(dico[i]);
+	}
+	*/
+	testPassword(dico[0]);
 });
