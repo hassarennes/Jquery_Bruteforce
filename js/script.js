512a5f48a0123d53f803d9cebe0db6daf36bbca9
hassarennes <hassanrennes@gmail.com>
2016-08-02 07:25:14 +0200

    rapatriement du dico

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-$(document).ready(function(){
-    var getPass = new XMLHttpRequest();
-    getPass.open("GET","http://docusland.fr/code-academie/js_bruteforce/data/dico.txt",true);
-    getPass.send();
-    console.log(getPass);
-
-
-})
+$.ajax({
+	url: "http://docusland.fr/code-academie/js_bruteforce/data/dico.txt",
+	complete: function(result){
+		dico = result.responseText.split("\n");
+	}
+});
